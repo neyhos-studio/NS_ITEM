dae9008b0d90a63963822a95b39d1daefee30610
Amaury <amaury.neyhouser@gmail.com>
2020-11-21 00:53:05 +0100

    Refactoring : HarvestingTool.cs HarvestingSickle.cs HarvestingAxe.cs HarvestingKnife.cs

---
+++ NS_ITEM/Items/Equipments/HarvestingTools/HarvestingTool.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NS_ITEMS.Items.Equipments.HarvestingTools
+{
+    public class HarvestingTool : Equipment
+    {
+
+    }
+}
---
+++ NS_ITEM/Items/Equipments/HarvestingTools/Types/HarvestingAxe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NS_ITEMS.Items.Equipments.HarvestingTools.Types
+{
+    public class HarvestingAxe : HarvestingTool
+    {
+    }
+}
