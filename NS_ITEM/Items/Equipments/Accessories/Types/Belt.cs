48a2dda53e2b609769d2d2ac675bcaea5cf99594
Amaury <amaury.neyhouser@gmail.com>
2020-11-21 00:15:26 +0100

    ITM-20 : Create Belt

---
+++ NS_ITEM/Items/Equipments/Accessories/Types/Belt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NS_ITEMS.Items.Equipments.Accessories.Types
+{
+    public class Belt : Accessory
+    {
+    }
+}
